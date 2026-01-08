6064c4a948e8c6d7984e0bc73fd73ad39ba9e2d3
Ravi <pravi1546@gmail.com>
2026-01-08 19:46:13 +0530

    added guss the output

---
+++ guss_the_ouput.js
@@ -0,0 +1,23 @@
+// const obj1 = {name:"jonh"}
+// const obj2 = obj1
+// obj2.name='doe'
+
+// console.log(obj2,obj1)
+
+// for (let i = 0; i < 5; i++) { 
+// setTimeout(function () { 
+// console.log(i); 
+// }, i * 1000);
+//  }
+
+// let a, b;
+// a = 5;
+// b = a
+// console.log( a);
+// console.log( b);
+
+
+// console.log(2 || 0)
+
+
+
